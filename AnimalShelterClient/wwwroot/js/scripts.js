9987e492513e3d3859283d09d8bfeabf2c69a13c
Mike Donovan <mike@mikedonovan.dev>
2023-04-01 11:08:05 -0700

    Add veil effect to body while modals show

--- AnimalShelterClient/wwwroot/js/scripts.js
+++ AnimalShelterClient/wwwroot/js/scripts.js
@@ -2,7 +2,9 @@ const showModal = modalId => {
   [...document.querySelectorAll(`.modal`)].forEach(modalElement => {
     modalElement.classList[modalElement.id !== modalId ? 'add' : 'remove']('obscured');
   });
+  document.querySelector("main").classList.add('veiled')
 }
 const hideModal = modalId => {
   document.getElementById(`${modalId}`).classList.add('obscured');
+  document.querySelector("main").classList.remove('veiled')
 }
