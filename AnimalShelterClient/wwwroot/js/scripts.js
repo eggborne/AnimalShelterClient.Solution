ec3f3583a56726fd044398029bb29b0673aa8969
Mike Donovan <mike@mikedonovan.dev>
2023-04-01 11:18:40 -0700

    Refine modal animations

--- AnimalShelterClient/wwwroot/js/scripts.js
+++ AnimalShelterClient/wwwroot/js/scripts.js
@@ -1,10 +1,18 @@
-const showModal = modalId => {
-  [...document.querySelectorAll(`.modal`)].forEach(modalElement => {
+const pause = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const showModal = async modalId => {
+  document.querySelector("main").classList.add('veiled');
+  [...document.querySelectorAll(`.modal`)].forEach(async modalElement => {
+    modalElement.classList[modalElement.id !== modalId ? 'add' : 'remove']('hidden');
+  });
+  await pause(2);
+  [...document.querySelectorAll(`.modal`)].forEach(async modalElement => {
     modalElement.classList[modalElement.id !== modalId ? 'add' : 'remove']('obscured');
   });
-  document.querySelector("main").classList.add('veiled')
 }
-const hideModal = modalId => {
+const hideModal = async modalId => {
+  document.querySelector("main").classList.remove('veiled');
   document.getElementById(`${modalId}`).classList.add('obscured');
-  document.querySelector("main").classList.remove('veiled')
+  await pause(300);
+  document.getElementById(`${modalId}`).classList.add('hidden');
 }
